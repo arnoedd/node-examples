c5d2f967f54f82146f38b374175cb5d9359ef760
arnoedd <arno@eightdoorsdown.com>
2016-03-22 15:19:38 -0700

    Create solve-2.js

---
+++ solve-2.js
@@ -0,0 +1,17 @@
+var rect = require('./rectangle-2');
+
+function solveRect(l,b) {
+  console.log("Solving for rectangle with l = " + " and b =" + b);
+  rect(l,b,function(err,rectangle){
+    if(err){
+      console.log(err);
+      }
+      else{
+        console.log("the area of a rectangle of dimensions lenght = " + l + " and breadth = " + b + 
+                    " is " + rectangle.area());
+         console.log("the perimeter of a rectangle of dimensions lenght = " + l + " and breadth = " + b + 
+                    " is " + rectangle.perimeter());
+                    }
+                    });
+                    }
+                    
