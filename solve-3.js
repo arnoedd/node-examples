18d82574b4ddb768521bde2365a81d8feec454b5
arnoedd <arno@eightdoorsdown.com>
2016-03-22 15:53:24 -0700

    Update solve-3.js

--- solve-3.js
+++ solve-3.js
@@ -5,3 +5,20 @@ var argv = require('yargs')
  
  var rect = require('./rectangle-2);
  
+ function solveRect(l,b) {
+  console.log("Solving for rectangle with l = " +l + " and b =" + b);
+  rect(l,b,function(err,rectangle){
+    if(err){
+      console.log(err);
+      }
+      else{
+        console.log("the area of a rectangle of dimensions lenght = " + l + " and breadth = " + b + 
+                    " is " + rectangle.area());
+         console.log("the perimeter of a rectangle of dimensions lenght = " + l + " and breadth = " + b + 
+                    " is " + rectangle.perimeter());
+                    }
+                    });
+                    }
+                    
+    solveRect(argv.l,argv.b);
+ 
